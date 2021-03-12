0149b9a93d3cd3dcb7ffd36957bcfc9713259e7d
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-03-12 11:04:38 +0100

    lesson 10

---
+++ lesson10/src/sandbox.ts
@@ -0,0 +1,29 @@
+// let greet: Function;
+
+
+// example 1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`)
+}
+
+// example 2
+let calc: (a: number, b: number, x: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+}
+
+// example 3
+let logDetails: (obj: { name: string, age: number }) => void;
+
+type person = { name: string, age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} os ${ninja.age} years old`);
+}
